451abbe0b9bd086038a4c5796c9586f8955b2c99
darcan94 <darcanf.94@gmail.com>
2023-11-01 15:08:46 -0300

    Solve 005_merge_two_sorted_list

--- 005_merge_two_sorted_list/Solution.java
+++ 005_merge_two_sorted_list/Solution.java
@@ -16,10 +16,22 @@
  */
 public class Solution {
         public static void main(String[] args) {
-            
+            ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+            System.out.println(l1.toString());
+            ListNode listsMerged = mergeTwoList(l1, l2);
+            System.out.println(listsMerged.toString());
         }
 
-        public static int[] mergeTwoList(int[] list1, int[] list2){
-            return new int[]{};
+        public static ListNode mergeTwoList(ListNode l1, ListNode l2){
+            if(l1 == null) return l2;
+            if(l2 == null) return l1;
+            if (l1.val < l2.val) {
+                l1.next = mergeTwoList(l1.next, l2);
+                return l1;
+            }
+
+            l2.next = mergeTwoList(l1, l2.next);
+            return l2;
         }
 }
